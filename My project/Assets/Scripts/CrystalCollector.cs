feb533bc59c3b16f5529e8f8cffdffe2dcb39bb0
Huila Gabriel Silva Dos Santos <100936330+HuilaSantos@users.noreply.github.com>
2025-11-24 12:00:08 -0300

    Adicionei um som de coleta aos cristais

--- My project/Assets/Scripts/CrystalCollector.cs
+++ My project/Assets/Scripts/CrystalCollector.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class CrystalCollector : MonoBehaviour
 {
+    [Header("Audio")]
+    [Tooltip("Sound played when a crystal is collected.")]
+    [SerializeField] private AudioClip collectSound;
+    [Range(0f,1f)] [SerializeField] private float collectVolume = 1f;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Crystal"))
         {
+            // Play one-shot audio at crystal position (3D if listener present).
+            if (collectSound != null)
+            {
+                AudioSource.PlayClipAtPoint(collectSound, other.transform.position, collectVolume);
+            }
             CrystalManager.Instance.CollectCrystal();
             Destroy(other.gameObject);
         }
